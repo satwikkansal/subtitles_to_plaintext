8d31060457dfab43e7d07aaf95a4f6e2eac3546f
Sajil C K <sajilck@gmail.com>
2023-02-08 07:38:32 +0530

    Make argument parser to process more SRT files
    - Make argument parser to process one or more SRT files
    - Split output text into multiple paragraphs

--- subtitle_to_text.py
+++ subtitle_to_text.py
@@ -1,17 +1,24 @@
 from sys import argv
 
-state = -1
-text_lines = []
-with open(argv[1],"r") as f:
-	for line in f.readlines():
-		if (line=="\n"):
-			state = -1
-			continue
-		elif state==1:
-			text_lines.append(line.strip())
-		else:
-			state += 1
+
+for file in argv[1:]:
+	state = -1
+	text_lines = []
+	fname = file.split('.srt')[0]
+	with open(file,"r") as f:
+		k = 0
+		for line in f.readlines():
+			k+=1
+			if (line=="\n"):
+				state = -1
+				continue
+			elif state==1:
+				text_lines.append(line.strip())
+			else:
+				state += 1
+			if k%14==0:
+				text_lines.append("\n\n")
 
 
-with open("converted_" + argv[1], 'w') as f:
-	f.write(" ".join(text_lines))
+	with open(fname+'.txt', 'w') as f:
+		f.write(" ".join(text_lines))
